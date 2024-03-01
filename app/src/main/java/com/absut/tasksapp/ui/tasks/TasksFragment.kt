7e06919649c92cce53ac37d2c7d383246f6e4ba1
Akshay <akshaynighot73@gmail.com>
2024-03-01 21:14:24 +0530

    implement viewpager

--- app/src/main/java/com/absut/tasksapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/absut/tasksapp/ui/tasks/TasksFragment.kt
@@ -3,26 +3,111 @@ package com.absut.tasksapp.ui.tasks
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.absut.tasksapp.R
+import com.absut.tasksapp.databinding.FragmentTasksBinding
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.launch
 
-class TasksFragment : Fragment() {
+class TasksFragment : Fragment(R.layout.fragment_tasks), MenuProvider {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val tabTitleArray = arrayOf(
+        "TODO",
+        "Completed"
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
+    }
+
+    inner class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+        override fun getItemCount(): Int {
+            return NUM_TABS
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            when (position) {
+                0 -> return TodoTaskFragment()
+                1 -> return CompletedTaskFragment()
+            }
+            return TodoTaskFragment()
+        }
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+      /*  val searchItem = menu.findItem(R.id.action_search)
+        searchView = searchItem.actionView as SearchView
+
+        val pendingQuery = viewModel.searchQuery.value
+        if (pendingQuery != null && pendingQuery.isNotEmpty()) {
+            searchItem.expandActionView()
+            searchView.setQuery(pendingQuery, false)
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.searchQuery.value = newText
+                return true
+            }
+        })*/
+
+       /* viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed).isChecked =
+                viewModel.preferenceFlow.first().hideCompleted
+        }*/
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false)
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+           /* R.id.action_hide_completed -> {
+                menuItem.isChecked = !menuItem.isChecked
+                viewModel.onHideCompletedClick(menuItem.isChecked)
+                true
+            }
+            R.id.action_delete_all_completed -> {
+                deleteAllCompletedTask()
+                true
+            }*/
+            else -> false
+        }
     }
+
 
     companion object {
-
+        private const val NUM_TABS = 2
     }
 }
