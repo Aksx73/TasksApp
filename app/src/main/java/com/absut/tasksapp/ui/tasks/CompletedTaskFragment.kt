c122d4d1b4453124bae175db0d44c949d3e50dc6
Akshay <akshaynighot73@gmail.com>
2024-03-04 22:06:59 +0530

    implement CompletedTaskFragment

--- app/src/main/java/com/absut/tasksapp/ui/tasks/CompletedTaskFragment.kt
+++ app/src/main/java/com/absut/tasksapp/ui/tasks/CompletedTaskFragment.kt
@@ -1,31 +1,69 @@
 package com.absut.tasksapp.ui.tasks
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.absut.tasksapp.R
+import com.absut.tasksapp.data.Task
+import com.absut.tasksapp.databinding.FragmentCompletedTaskBinding
+import com.absut.tasksapp.databinding.FragmentTodoTaskBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class CompletedTaskFragment : Fragment() {
+class CompletedTaskFragment : Fragment(),TaskAdapter.OnItemClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentCompletedTaskBinding? = null
+    private val binding get() = _binding!!
 
+    private val viewModel by activityViewModels<TaskViewModel>()
+
+    private val taskAdapter: TaskAdapter by lazy {
+        TaskAdapter(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_completed_task, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentCompletedTaskBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    /*private fun deleteAllCompletedTask() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply{
+            recyclerView.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+
+        viewModel.completedTasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+            binding.emptyView.isVisible = it.isEmpty()
+        }
+    }
+
+    override fun onItemClick(task: Task) {
+        val action = TasksFragmentDirections.actionTaskFragmentToAddEditTaskFragment(task)
+        findNavController().navigate(action)
+    }
+
+    override fun onCheckBoxClick(task: Task, isChecked: Boolean) {
+        viewModel.onTaskCheckedChanged(task, isChecked)
+    }
+
+    private fun deleteAllCompletedTask() {
         MaterialAlertDialogBuilder(requireActivity())
             .setTitle("Delete all completed task?")
             .setMessage("This action cannot be undone")
@@ -34,7 +72,7 @@ class CompletedTaskFragment : Fragment() {
             }
             .setNegativeButton("Cancel", null)
             .show()
-    }*/
+    }
 
     companion object {
 
