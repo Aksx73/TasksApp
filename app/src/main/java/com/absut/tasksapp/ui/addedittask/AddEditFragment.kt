a873c30f748b1ad38454be348f6b9ba80b13fcbd
Akshay <akshaynighot73@gmail.com>
2024-03-04 22:12:57 +0530

    implement view for AddEditFragment

--- app/src/main/java/com/absut/tasksapp/ui/addedittask/AddEditFragment.kt
+++ app/src/main/java/com/absut/tasksapp/ui/addedittask/AddEditFragment.kt
@@ -5,23 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.absut.tasksapp.R
+import com.absut.tasksapp.databinding.FragmentAddEditBinding
+import com.absut.tasksapp.databinding.FragmentTodoTaskBinding
+import com.absut.tasksapp.ui.tasks.TaskViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AddEditFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentAddEditBinding? = null
+    private val binding get() = _binding!!
 
-    }
+    private val viewModel by activityViewModels<TaskViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit, container, false)
+        _binding = FragmentAddEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
