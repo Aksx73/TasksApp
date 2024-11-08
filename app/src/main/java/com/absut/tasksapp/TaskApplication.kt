a6864259e48346a36252b5b265515a58b2e481de
Akshay <akshaynighot73@gmail.com>
2024-11-08 18:49:06 +0530

    Added code to create channel in Application class

--- app/src/main/java/com/absut/tasksapp/TaskApplication.kt
+++ app/src/main/java/com/absut/tasksapp/TaskApplication.kt
@@ -1,7 +1,32 @@
 package com.absut.tasksapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TaskApplication : Application()
+class TaskApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Timed tasks"
+            val descriptionText = "channel for all timed tasks"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("timed_tasks", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
