726e678eb84d3dd92b489989908d5cccd4e27416
Akshay <akshaynighot73@gmail.com>
2024-11-09 21:27:00 +0530

    createNotificationChannel() function removed from TaskApplication.kt class

--- app/src/main/java/com/absut/tasksapp/TaskApplication.kt
+++ app/src/main/java/com/absut/tasksapp/TaskApplication.kt
@@ -8,29 +8,4 @@ import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TaskApplication : Application(){
-
-    override fun onCreate() {
-        super.onCreate()
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
-                description = CHANNEL_DESCRIPTION
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    companion object {
-        const val CHANNEL_NAME = "Timed tasks"
-        const val CHANNEL_DESCRIPTION = "channel for all timed tasks"
-        const val CHANNEL_ID = "channel_timed_tasks"
-    }
-}
+class TaskApplication : Application()
