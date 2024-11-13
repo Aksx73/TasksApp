89ddd9dfeab33a9bdb92e92161faed12ca9f9b0f
AkshayBJS <anighot@bjsindia.org>
2024-11-13 11:59:24 +0530

    commented compose dependencies because of gradle build issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
     id("androidx.navigation.safeargs") version "2.8.3" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
+    //id("org.jetbrains.kotlin.plugin.compose") version "2.0.10"
 }
