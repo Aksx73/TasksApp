9e7f5311b89974e728ea871ecbe96d75bf18650a
Akshay <akshaynighot73@gmail.com>
2024-11-13 07:54:32 +0530

    bumped AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("androidx.navigation.safeargs") version "2.8.3" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
