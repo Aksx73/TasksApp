24258060162b5624b3a29e5d570a576ecea3b66c
Akshay <akshaynighot73@gmail.com>
2024-11-15 20:41:18 +0530

    bumped dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.10" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
-    id("androidx.navigation.safeargs") version "2.8.3" apply false
+    id("androidx.navigation.safeargs") version "2.8.4" apply false
     id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
     //id("org.jetbrains.kotlin.plugin.compose") version "2.0.10"
 }
